828497ce5d1eba2d1e7098f037d107dd10e1bf1d
LilaSarfson <lilamandarina@hotmail.com>
2022-03-28 10:13:29 +0200

    hecho?

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 //generate random numbers
-let firstNumber = parseInt(Math.random()*10);
-let secondNumber = parseInt(Math.random()*10);
+let firstNumber = parseInt(Math.random() * 10);
+let secondNumber = parseInt(Math.random() * 10);
 
 //get the total
 let total = firstNumber + secondNumber;
 
 //display numbers on the canvas
-let primary = document.getElementById('primary-number');
-    primary.innerHTML = `<p>${firstNumber}</p>`;
+let primary = $('#primary-number');
+primary.html(`<p>${firstNumber}</p>`);
 
-let secondary = document.getElementById('secondary-number');
-    secondary.innerHTML = `<p>${secondNumber}</p>`
+let secondary = $('#secondary-number');
+secondary.html(`<p>${secondNumber}</p>`);
 
 
 //get guess from user
-let button = document.getElementById('btn')
+let button = $('#btn');
 
-button.addEventListener('click', function(){
+button.click(function () {
 
-let guess = document.getElementById('guess').value;
+    let guess = $('#guess').val();
     guess = Number(guess);
-//check answer
-if (guess === total){
-    alert('Correct');
-    window.location.reload()
-} else {
-    alert('Sorry. Incorrect. The correct answer was ' + total + '.')
-    window.location.reload()
+    //check answer
+    if (guess === total) {
+        alert('Correct');
+        window.location.reload()
+    } else {
+        alert('Sorry. Incorrect. The correct answer was ' + total + '.')
+        window.location.reload()
 
-}
-    });
+    }
+});
+
